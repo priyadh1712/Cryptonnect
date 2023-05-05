3de2970f5f77a58ec1db29e0f1083f65d99eebbc
priyadh1712 <99671393+priyadh1712@users.noreply.github.com>
2023-05-05 15:56:47 +0530

    Add files via upload

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import logo from "../../images/Novoice_Logo.png";
+
+const NavBarItem = ({ title, classprops }) => (
+  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+);
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" width="100px" height="200px" className="logo" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
